dc1d584e26a2fd9dc2192935bbe2ef251dcd1678
shamilaliyev <saliyev17610@ada.edu.az>
2026-06-11 18:27:18 +0400

    Fix Swagger OpenAPI schema

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
 from app.database import init_db
 from app.routers import auth, earthquakes
 
@@ -33,20 +32,3 @@ def root():
 app.include_router(auth.router)
 app.include_router(earthquakes.router)
 
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(app, title=app.title, version=app.version, description=app.description)
-    # Ensure an OAuth2 password flow is available in the OpenAPI components
-    components = openapi_schema.setdefault("components", {})
-    security_schemes = components.setdefault("securitySchemes", {})
-    security_schemes.setdefault("OAuth2Password", {
-        "type": "oauth2",
-        "flows": {"password": {"tokenUrl": "/auth/login", "scopes": {}}},
-    })
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
